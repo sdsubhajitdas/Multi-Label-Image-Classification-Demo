84d617665c4d809774842183390592383b36eabc
Subhajit Das <jeetudas95@gmail.com>
2020-04-07 04:29:53 +0530

    Debug Mode turned off

--- demo/__init__.py
+++ demo/__init__.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "09167a185e72cd1619eb65beb0b623cb"
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 MODEL_PATH = os.path.join(app.root_path,'static', 'model', 'demo_model.h5')
 CLASSES = "Aeroplane,Bicycle,Bird,Boat,Bottle,Bus,Car,Cat,Chair,Cow,Diningtable,Dog,Horse,Motorbike,Person,Pottedplant,Sheep,Sofa,Train,Tvmonitor".split(",")
 
